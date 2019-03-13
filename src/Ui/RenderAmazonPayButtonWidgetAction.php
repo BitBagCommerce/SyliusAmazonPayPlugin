f805ead0f0da53ae897c68e747c0f11a9f5ea4e8
Konrad Alfaro <alfaro.konrad@gmail.com>
2019-03-13 13:25:38 +0100

    Refactored AmazonPay render button controller

--- src/Ui/RenderAmazonPayButtonWidgetAction.php
+++ src/Ui/RenderAmazonPayButtonWidgetAction.php
@@ -5,19 +5,17 @@ declare(strict_types=1);
 namespace Tierperso\SyliusAmazonPayPlugin\Ui;
 
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
-
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 final class RenderAmazonPayButtonWidgetAction
 {
-    /** @var ParameterBagInterface */
-    private $environment;
-
     /** @var EngineInterface */
     private $templatingEngine;
 
-    public function __construct(EngineInterface $templatingEngine, ParameterBagInterface $environment)
+    /** @var string */
+    private $environment;
+
+    public function __construct(EngineInterface $templatingEngine, string $environment)
     {
         $this->templatingEngine = $templatingEngine;
         $this->environment = $environment;
@@ -26,23 +24,24 @@ final class RenderAmazonPayButtonWidgetAction
     public function __invoke(): Response
     {
         // TODO fetch data of AmazonPay Payment Gateway
-
-        if ($this->environment->get('kernel.environment') === "dev" ) {
-            $api_env = '/sandbox';
-        }
-        elseif ($this->environment->get('kernel.environment') === "prod") {
-            $api_env = '';
-        }
-
         return $this->templatingEngine->renderResponse(
             'TierpersoSyliusAmazonPayPlugin:AmazonPay:buttonWidget.html.twig',
             [
                 'amazon' => [
-                    'api_env' => $api_env,
+                    'amazonApiEnvironment' => $this->getAmazonApiEnvironment(),
                     'clientId' => 'amzn1.application-oa2-client.46d27733e6044b08860955d63511ad19',
                     'merchantId' => 'A5445N2KWSM0Z',
                 ],
             ]
         );
+    }
+
+    private function getAmazonApiEnvironment(): ?string
+    {
+        if ('dev' === $this->environment) {
+            return '/sandbox';
+        }
+
+        return null;
     }
 }
