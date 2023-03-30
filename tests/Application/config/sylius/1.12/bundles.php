6e2f303db951cebb18980477edcfd82cb388589f
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-30 06:57:34 +0200

    ECS

--- tests/Application/config/sylius/1.12/bundles.php
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file was created by developers working at BitBag
  * Do you need more information about us and what we do? Visit our https://bitbag.io website!
@@ -10,5 +12,5 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
     Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
-    League\FlysystemBundle\FlysystemBundle::class => ['all' => true]
+    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
 ];
